579a6c1c5b97726c3d23122ca587902e544853ba
Osvaldo Martini <osvaldo.martini@gmail.com>
2024-09-22 18:09:16 +0200

    Update GridItem.tsx

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -28,6 +28,7 @@ const groupByBlock = (data: BlockLoopInstructionLoadDTO[]) => {
 // Helper function to reassign instructionOrderNumber starting from 1 within each block
 const reassignInstructionOrderNumbersByBlock = (instructions: BlockLoopInstructionLoadDTO[]) => {
   // Group instructions by blockId
+  console.log("reassignInstructionOrderNumbersByBlock: ");
   const grouped = groupByBlock(instructions);
 
   // Iterate over each block and reassign instructionOrderNumbers
@@ -93,6 +94,7 @@ const GridItem: React.FC<GridItemProps> = ({ data }) => {
       const blockInstructionIndex = blockInstructions.findIndex(instruction => instruction.id === instructionId);
 
       // Ensure that the instruction isn't already the first one within its block
+      console.log("blockInstructionIndex", blockInstructionIndex);
       if (blockInstructionIndex > 0) {
         const previousInstruction = blockInstructions[blockInstructionIndex - 1];
 
@@ -124,54 +126,57 @@ const GridItem: React.FC<GridItemProps> = ({ data }) => {
 
   return (
     <div className="grid-container">
-      {Object.entries(groupedData).sort(([aId], [bId]) => parseInt(aId) - parseInt(bId)).map(([blockId, blockData]) => (
-        <div key={blockId} className="block">
-          {/* Block header with garbage, up, and down buttons */}
-          <div className="block-header">
-            <span>{blockId}</span>
-            <span>{blockData.blockName}</span>
-            <span>Instruction Count: {blockData.instructions.length}</span>
-            <div className="move-buttons">
-              {/* Add the garbage button click handler */}
-              <img
-                src="../garbage.png"
-                className="garbage-button"
-                onClick={() => handleRemoveBlock(Number(blockId))}
-              />
-              <img src="../up.png" className="move-button" />
-              <img src="../down.png" className="move-button" />
+      {Object.entries(groupedData)
+        // Sort by blockOrderNumber instead of blockId
+        .sort(([, aBlockData], [, bBlockData]) => aBlockData.instructions[0].blockOrderNumber - bBlockData.instructions[0].blockOrderNumber)
+        .map(([blockId, blockData]) => (
+          <div key={blockId} className="block">
+            {/* Block header with garbage, up, and down buttons */}
+            <div className="block-header">
+              <span>{blockId}</span>
+              <span>{blockData.blockName}</span>
+              <span>Instruction Count: {blockData.instructions.length}</span>
+              <div className="move-buttons">
+                {/* Add the garbage button click handler */}
+                <img
+                  src="../garbage.png"
+                  className="garbage-button"
+                  onClick={() => handleRemoveBlock(Number(blockId))}
+                />
+                <img src="../up.png" className="move-button" />
+                <img src="../down.png" className="move-button" />
+              </div>
+            </div>
+            <div className="instructions-list">
+              {blockData.instructions.map((instruction) => (
+                <div key={instruction.id} className="instruction-item">
+                  <span>{instruction.id}</span>
+                  <span>{instruction.instructionOrderNumber}</span>
+                  <span>{instruction.instructionType}</span>
+                  <span>{instruction.name}</span>
+                  <span>{instruction.description}</span>
+                  <div className="move-buttons">
+                    <img src="../up.png" className="move-button"
+                      onClick={() => handleMoveUp(instruction.id)}
+                    />
+                    <img
+                      src="../down.png"
+                      className="move-button"
+                      onClick={() => handleMoveDown(instruction.id)}
+                    />
+                    <img src="../edit.png" className="edit-button" />
+                    {/* Add the cross button click handler */}
+                    <img
+                      src="../cross.png"
+                      className="cross-button"
+                      onClick={() => handleRemoveInstruction(instruction.id)}
+                    />
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
-          <div className="instructions-list">
-            {blockData.instructions.map((instruction) => (
-              <div key={instruction.id} className="instruction-item">
-                <span>{instruction.id}</span>
-                <span>{instruction.instructionOrderNumber}</span>
-                <span>{instruction.instructionType}</span>
-                <span>{instruction.name}</span>
-                <span>{instruction.description}</span>
-                <div className="move-buttons">
-                  <img src="../up.png" className="move-button"
-                    onClick={() => handleMoveUp(instruction.id)}
-                  />
-                  <img
-                    src="../down.png"
-                    className="move-button"
-                    onClick={() => handleMoveDown(instruction.id)}
-                  />
-                  <img src="../edit.png" className="edit-button" />
-                  {/* Add the cross button click handler */}
-                  <img
-                    src="../cross.png"
-                    className="cross-button"
-                    onClick={() => handleRemoveInstruction(instruction.id)}
-                  />
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
