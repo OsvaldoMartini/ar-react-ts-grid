ff97219e4b77541908caaea6b8f8729a5191982e
Osvaldo Martini <osvaldo.martini@gmail.com>
2025-03-20 17:30:48 +0000

    SHADOW ROOT element CssSelector

--- scripts/script-hover-pick.js
+++ scripts/script-hover-pick.js
@@ -592,7 +592,7 @@
       shadowHost: shadowHostSelector,
       shadowRoot: shadowRoot ? true : false,
       nestedShadow: shadowPath.length > 1, // Detects if multiple shadow roots are involved
-      cssSelector: cssSelector, // Correct CSS selector for nested shadow elements
+      cssSelector: elementCssSelector, // cssSelector shadowRoot
     };
 
     // Store tagName and other details in the Map
