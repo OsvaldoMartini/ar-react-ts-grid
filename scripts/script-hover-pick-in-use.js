d0fb23189ac39a4fc7f02184e6e833e670f3e43b
Osvaldo Martini <osvaldo.martini@gmail.com>
2026-02-10 18:19:24 +0100

    Update script-hover-pick-in-use.js

--- scripts/script-hover-pick-in-use.js
+++ scripts/script-hover-pick-in-use.js
@@ -1273,25 +1273,25 @@
   // });
 
   // window.cloneTerms = null; // Invalidating the function
-  // })(
-  //   arguments[0],
-  //   arguments[1],
-  //   arguments[2],
-  //   arguments[3],
-  //   arguments[4],
-  //   arguments[5],
-  //   arguments[6],
-  //   arguments[7],
-  //   arguments[8],
-  // );
 })(
-  false,
-  50869,
-  "scannerTool",
-  "scannerGrid",
-  "addPickOne",
-  2,
-  66,
-  "https://www.inlinea.ch/",
-  "https://www.inlinea.ch/",
+  arguments[0],
+  arguments[1],
+  arguments[2],
+  arguments[3],
+  arguments[4],
+  arguments[5],
+  arguments[6],
+  arguments[7],
+  arguments[8],
 );
+// })(
+//   false,
+//   50869,
+//   "scannerTool",
+//   "scannerGrid",
+//   "addPickOne",
+//   2,
+//   66,
+//   "https://www.inlinea.ch/",
+//   "https://www.inlinea.ch/",
+// );
